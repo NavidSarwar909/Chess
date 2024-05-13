34070bd6abe5014011eab860631c9c54a77b92dc
Navid Sarwar <navidsarwar@hotmail.com>
2024-05-13 17:39:52 -0400

    Creating skeleton for classes

---
+++ ChessGame.py
@@ -0,0 +1,48 @@
+# start by making all the backend stuff and object (Board, All piece classes)
+class board():
+    # Constructor
+    def __init__(self):
+        pass
+
+class piece():
+    # Constructor
+    def __init__(self):
+        pass
+
+class rook(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+class bishop(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+class knight(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+class queen(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+class king(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+class pawn(piece):
+    # Constructor
+    def __init__(self):
+        pass
+
+# class will represent a game
+class game():
+    # Constructor
+    def __init__(self):
+        # create a board
+        gameBoard = board()
+        
